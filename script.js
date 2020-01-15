7d4fd028c607d095deb02bb29df13b089eceee2e
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-01-15 14:39:43 +0000

    Added another sum in script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Define Arrow Function
 
 const sum = (num1, num2) => {
-    num1 + num2;
+    return num1 + num2;
 };
 
 // Set Up Variable
 let txt = "";
-txt+= "Getting sums";
-txt+= "The sum of 10 and 20 is " + sum(10, 20);
+txt+= "Getting sums" + "<br>";
+txt+= "The sum of 10 and 20 is " + sum(10, 20) + "<br>";
+txt+= "The sum of 4.5 and 3.2 is " + sum(4.5, 3.2);
 
 // Display txt in Browser
 document.getElementById("demo").innerHTML = txt;
