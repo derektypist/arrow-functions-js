feef6e25495d225b28ea9df69720c866eed765a7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-01-15 14:33:19 +0000

    Changed name in function at script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Define Arrow Function
 
-const getSum = (num1, num2) => {
+const sum = (num1, num2) => {
     num1 + num2;
 };
 
 // Set Up Variable
 let txt = "";
 txt+= "Getting sums";
-txt+= "The sum of 10 and 20 is " + getSum(10, 20);
+txt+= "The sum of 10 and 20 is " + sum(10, 20);
 
 // Display txt in Browser
 document.getElementById("demo").innerHTML = txt;
