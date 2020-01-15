5bd7b2f692f83941b63c8290a7ec1f44579afed9
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-01-15 14:46:28 +0000

    Added treble function at script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Define Arrow Function
+// Define Arrow Functions
 
 const sum = (num1, num2) => {
     return num1 + num2;
+};
+
+
+const treble = (x) => {
+    return x * 3;
 };
 
 // Set Up Variable
 let txt = "";
 txt+= "Getting sums" + "<br>";
 txt+= "The sum of 10 and 20 is " + sum(10, 20) + "<br>";
-txt+= "The sum of 4.5 and 3.2 is " + sum(4.5, 3.2);
+txt+= "The sum of 4.5 and 3.2 is " + sum(4.5, 3.2) + "<br>";
+txt+= "<br>";
+txt+= "Trebling" + "<br>";
+txt+= "4 times 3 is " + treble(4) + "<br>";
+txt+= "5.5 times 3 is " + treble(5.5) + "<br>";
 
 // Display txt in Browser
 document.getElementById("demo").innerHTML = txt;
